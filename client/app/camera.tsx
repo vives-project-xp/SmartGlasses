ac16f94631700807149683840a139935e6432306
TimoPlts <timoplets@gmail.com>
2025-10-16 08:39:46 +0200

    update live-page

--- client/app/camera.tsx
+++ client/app/camera.tsx
@@ -1,12 +1,15 @@
+import { Button } from "@/components/Button";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
 import { router } from "expo-router";
 import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import { Button } from "@/components/Button";
+
+
 
 export default function CameraScreen() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+  const [showTranslation, setShowTranslation] = useState(false);  // voor vertalingd
 
   // Request permission automatically on mount so camera opens when entering screen
   useEffect(() => {
@@ -52,16 +55,26 @@ export default function CameraScreen() {
   return (
     <View className="flex-1 bg-[#F2F2F2]">
       <View className="flex-1 items-center justify-center px-5">
-        <View 
-          className="w-full max-w-full rounded-2xl overflow-hidden bg-black border border-[#B1B1B1]"
+        <View // Camera 
+          className="w-full max-w-full rounded-2xl overflow-hidden bg-black border border-[#B1B1B1] mb-3"
           style={{ aspectRatio: 1, borderStyle: "dashed" }}
         >
           <CameraView facing={facing} className="w-full h-full" />
         </View> 
+
+        {showTranslation && ( // Vertaling
+          <View className="w-full bg-white rounded-xl border border-[#B1B1B1] p-6 mb-5">
+            <Text className="text-black text-xl font-semibold text-center" style={{ aspectRatio: 2 }}>
+              Vertaling
+            </Text>
+          </View>
+        )}
+
       </View> 
+      
 
       {/* Overlay controls */}
-      <View pointerEvents="box-none" className="absolute inset-0 justify-end">
+      <View pointerEvents="box-none" className="absolute left-0 right-0 bottom-0">
         <View className="items-center bg-white px-4 py-3 self-stretch">
           <View className="flex-row justify-center gap-3">
             <Button
@@ -69,6 +82,14 @@ export default function CameraScreen() {
               className="bg-white px-10 py-4 border-2 rounded-lg border-[#B1B1B1]"
               labelClasses="text-black text-lg font-semibold"
               onPress={() => router.back()}
+              size="lg"
+              variant="secondary"
+            />
+            <Button
+              label="Text"
+              className="bg-white px-5 py-4 border-2 rounded-lg border-[#B1B1B1]"
+              labelClasses="text-black text-lg font-semibold"
+              onPress={() => setShowTranslation((v) => !v)}
               size="lg"
               variant="secondary"
             />
